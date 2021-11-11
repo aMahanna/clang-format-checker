09d56d3857c5e0db2293c153a0056931e8c2c3f2
aMahanna <anthony.mahanna@gmail.com>
2021-11-11 11:30:26 -0500

    release 1.1.0 prep

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # clang-format-checker
 
+Checks your files using the `.clang-format` of your repo. Fails on any clang-format error.
+
 Create a `clang-format-checker.yml` file in `.github/workflows/`.
 Paste this code into the file:
 
@@ -12,7 +14,37 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v2
-      - uses: aMahanna/clang-format-checker@v1.0.1
+      - uses: aMahanna/clang-format-checker@v1.1.0
         with:
-          sources: "src/*.cpp,src/*.h"
+          sources: "src/Card.h src/Card.cpp lib/**/*.h"
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Common use case:
+
+See https://github.com/Ana06/get-changed-files
+
+```yml
+name: clang-format-checker
+on: push
+jobs:
+  lint:
+    name: clang-format-checker
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: Ana06/get-changed-files@v2.0.0
+        id: gcf
+        with:
+          filter: |
+            **/*.cpp
+            **/*.hpp
+            **/*.c
+            **/*.h
+      - uses: aMahanna/clang-format-checker@v1.1.0
+        with:
+          sources: "${{ steps.gcf.outputs.added_modified }}"
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,18 @@
 #!/bin/bash
-SOURCES=()
-UNFORMATTED_FILES=()
-EXIT_STATUS=0
+
+set -e
 
 function log() {
     echo -e "[ gh-action ] :: $1"
 }
 
-function split_csv() {
-    IFS=','
-    csv_data="$1"
-    local -n global_list_array="$2"
-    for i in $csv_data; do
-        if [ -f "$i" ]; then
-            global_list_array+=("$i")
-        fi
-    done
-    unset IFS
-}
-
-function check_file() {
-    local file="$1"
-    message="$(clang-format -n -Werror --ferror-limit=1 -style=file "${file}")"
-    local status="$?"
-    if [ $status -ne 0 ]; then
-        echo "$message" >&2
-        EXIT_STATUS=1
-        return 1
-    fi
-    return 0
-}
-
 cd "$GITHUB_WORKSPACE" || exit 2
-log "Action started"
-log "Sources to check: $INPUT_SOURCES\n"
-split_csv "$INPUT_SOURCES" SOURCES
 
-for file in "${SOURCES[@]}"; do
-    check_file "$file"
-    if [ $? -ne 0 ]; then
-        UNFORMATTED_FILES+=("$file")
-    fi
-done
-
-if [ $EXIT_STATUS -eq 0 ]; then
-    log "Congrats! The sources are clang formatted."
-    exit 0
+if [ -z "$INPUT_SOURCES" ]
+then
+    log "\$INPUT_SOURCES is empty. Skipping Action..."
 else
-    log "Some file is not formatted correctly."
-    log "You might want to run: "
-    for ((i = 0; i < ${#UNFORMATTED_FILES[@]}; i++)); do
-        if [ $i -ne 0 ]; then
-            echo -n " && "
-        fi
-        echo "clang-format -style=file -i "${UNFORMATTED_FILES[$i]}" \\"
-    done
-    exit 1
+    log "Action started"
+    log "Sources to check: $INPUT_SOURCES\n"
+    clang-format -n -Werror -style=file $INPUT_SOURCES
 fi
