8b84730d2dc4f0c888e407c3c7aabadcdacbcf3c
aMahanna <anthony.mahanna@gmail.com>
2021-12-03 22:26:27 -0500

    cleanup action

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM alpine:3.12
 ENV clang_version=10.0.0-r2
-LABEL description="Clang Format ${clang_version}"
-RUN apk add --no-cache clang=${clang_version} git bash
-COPY entrypoint.sh /entrypoint.sh
+RUN apk add --no-cache clang=${clang_version}
+COPY entrypoint.sh .
 RUN chmod +x entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
-
-set -e
-
-function log() {
-    echo -e "[ gh-action ] :: $1"
-}
-
+#!/bin/sh
 cd "$GITHUB_WORKSPACE" || exit 2
-
 if [ -z "$INPUT_SOURCES" ]
 then
     log "\$INPUT_SOURCES is empty. Skipping Action..."
 else
-    log "Action started"
-    log "Sources to check: $INPUT_SOURCES\n"
+    echo "-----------"
+    clang-format --version
+    echo "-----------"
+    echo "clang-format -n -Werror -style=file $INPUT_SOURCES"
+    echo "-----------"
     clang-format -n -Werror -style=file $INPUT_SOURCES
+    echo "erledigt"
 fi
